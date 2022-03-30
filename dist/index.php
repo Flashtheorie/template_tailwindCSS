38f1def39df185e67e6f2c9c1f54359713860dc9
Antoine Pascual <74872926+Flashtheorie@users.noreply.github.com>
2022-03-30 13:21:06 +0200

    Ajout de tout les fichiers

---
+++ dist/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>title</title>
+  <meta name="author" content="name">
+  <meta name="description" content="description here">
+  <meta name="keywords" content="keywords,here">
+  <link rel="stylesheet" href="css/tailwind.css" type="text/css">
+ <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+ <script src="https://kit.fontawesome.com/a513ca5fbe.js" crossorigin="anonymous"></script>
+  </head>
+  
+  <body class="bg-gray-100">
+<?php include 'include/navbar.php'; ?>
+<br>
+    <div class="container mx-auto">
+      <h1 class="text-3xl lg:text-6xl">Hello World!</h1>
+    </div>
+    
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script> 
+  
+  </body>
+  
+</html>
